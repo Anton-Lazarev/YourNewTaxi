dcdfe46958955ec8aacae76e5716aeab5bf3e736
FenixFly <uegene.unn@gmail.com>
2015-05-04 11:45:27 +0300

    Создание проектов с  настроенными зависимостями

---
+++ YourNewTaxi_source_code/YourNewTaxi.Domain/Entities/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourNewTaxi.Domain.Entities
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+
+        public string AddressFinish { get; set; }
+        public string AddressStart { get; set; }
+        public string Date { get; set; }
+        public int ExecutorID { get; set; }
+        public string FIO { get; set; }
+        public double Price { get; set; }
+        public string Status { get; set; }
+        public string Time { get; set; }
+
+    }
+}
