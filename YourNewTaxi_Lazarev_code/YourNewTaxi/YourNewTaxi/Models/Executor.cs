50037db0ba32118b9d19ac3521a7bb7c177d7d6e
Anton Lazarev <alone2904@gmail.com>
2015-05-11 17:18:43 +0300

    Сделал отдельный проект с рабочим контроллером и виюшкой клиента. Единственное, что пока не до конца реализовано - проверка на валидацию входящих данных. Некорректно проверяет время.

---
+++ YourNewTaxi_Lazarev_code/YourNewTaxi/YourNewTaxi/Models/Executor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YourNewTaxi.Models
+{
+    public class Executor
+    {
+        public string AvtoInfo { get; set; }
+        public string FIO { get; set; }
+        public int ID { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ YourNewTaxi_Lazarev_code/YourNewTaxi/YourNewTaxi/Models/Order.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YourNewTaxi.Models
+{
+    public class Order
+    {
+        [Required(ErrorMessage = "Please enter finish address")]
+        public string AddressFinish { get; set; }
+
+        [Required(ErrorMessage = "Please enter start address")]
+        public string AddressStart { get; set; }
+
+        [RegularExpression("[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])",
+            ErrorMessage = "Please enter a valid date")]
+        public string Date { get; set; }
+
+        public int ExecutorID { get; set; }
+        public string FIO { get; set; }
+        public int ID { get; set; }
+        public double Price { get; set; }
+        public string Status { get; set; }
+
+        [RegularExpression("[0-9]{0,24}:[0-9]{0,60}",
+            ErrorMessage = "Please enter a valid time")]
+        public string Time { get; set; }
+
+        /*[RegularExpression((?:8|\+7)? ?\(?(\d{3})\)? ?(\d{3})[ -]?(\d{2})[ -]?(\d{2}),
+          ErrorMessage = "Please enter a valid telephone number")]*/
+        public string ClientPhone { get; set; }
+
+        public void AddOrder(Order order)
+        {
+            //Делаем запись в БД
+        }
+    }
+}
